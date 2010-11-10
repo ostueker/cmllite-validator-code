9a321067334369ee1c2b3fca1ea46776cab75610
gigadot <gigadot@localhost>
2010-11-10 17:53:35 +0000

    Added more tests for cmlcomp.

--- src/test/java/org/xmlcml/www/cmlcomp/InvalidCMLCompTest.java
+++ src/test/java/org/xmlcml/www/cmlcomp/InvalidCMLCompTest.java
@@ -25,26 +25,6 @@ public class InvalidCMLCompTest extends CMLCompTester {
 
     @Test
     public void testIndividual() {
-        // test a cmlcomp joblist which contains two empty jobs with two unique titles.
-        test("joblist-two-empty-jobs-with-two-titles.cml");
+        test("one-job-empty-init.cml");
     }
-//
-//    @Test
-//    //@Ignore
-//    public void testAll() {
-//        // test a cmlcomp which has a module of unknown role.
-//        test("undefined-module.cml");
-//        // test a cmlcomp which has a molecule under cml root
-//        test("undefined-molecule.cml");
-//        // test a cmlcomp which has an empty joblist
-//        test("empty-joblist.cml");
-//        // test a cmlcomp joblist which contains an empty job (+no title).
-//        test("joblist-one-empty-job.cml");
-//        // test a cmlcomp joblist which contains an empty job but titled).
-//        test("joblist-one-empty-job-with-title.cml");
-//        // test a cmlcomp joblist which contains two empty jobs (no title).
-//        test("joblist-two-empty-jobs.cml");
-//        // test a cmlcomp joblist which contains two empty jobs with one title.
-//        test("joblist-two-empty-jobs-with-one-title.cml");
-//    }
 }
